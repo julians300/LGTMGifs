1c0c3e224e66aca135c3d4cfa81a58cb65f84330
Julian Salas <julians300@gmail.com>
2022-07-03 15:37:33 -0700

    loading color

--- components/GifList/GifItem.tsx
+++ components/GifList/GifItem.tsx
@@ -1,24 +1,10 @@
 import React from "react";
 import slugify from "slugify";
 import NextLink from "next/link";
-import { Box, Image, AspectRatio, Text, Stack, Flex, Link, Skeleton, useColorMode } from "@chakra-ui/react";
+import { Box, AspectRatio, Text, Stack, Flex, Link, useColorMode } from "@chakra-ui/react";
 import CopyLinks from "./CopyLinks";
 import { Gif } from "../../types/Gif";
 import NextImage from "next/image";
-import { getImageSize } from "next/dist/server/image-optimizer";
-
-const loadingColors = {
-  0: { colorStart: "pink", colorEnd: "orange" },
-  1: { colorStart: "green", colorEnd: "blue" },
-  2: { colorStart: "orange", colorEnd: "purple" },
-  3: { colorStart: "gray", colorEnd: "blue" },
-  4: { colorStart: "blue", colorEnd: "red" },
-  5: { colorStart: "pink", colorEnd: "cyan" },
-  6: { colorStart: "green", colorEnd: "orange" },
-  7: { colorStart: "blue", colorEnd: "green" },
-  8: { colorStart: "green", colorEnd: "yellow" },
-  9: { colorStart: "teal", colorEnd: "orange" },
-};
 
 interface Props {
   gif: Gif;
@@ -33,11 +19,9 @@ const GifItem = ({ gif, index }: Props) => {
         <NextLink href={`/gifs/${slugify(gif.name.toLowerCase())}`}>
           <Link href={`/gifs/${slugify(gif.name.toLowerCase())}`} display={"inline-block"} w={"100%"} rounded={"4px"}>
             <AspectRatio ratio={1.25 / 1}>
-              <AspectRatio ratio={1.25 / 1}>
-                <Box m={0} border={0} rounded={"4px"} w="100%" height="100%">
-                  <NextImage layout="fill" objectFit={"cover"} src={gif.thumbnail.url} />
-                </Box>
-              </AspectRatio>
+              <Box m={0} border={0} rounded={"4px"} w="100%" height="100%" bg="gray.100">
+                <NextImage layout="fill" objectFit={"cover"} src={gif.thumbnail.url} />
+              </Box>
             </AspectRatio>
           </Link>
         </NextLink>
